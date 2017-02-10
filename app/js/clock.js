797c4a342f012f6e5ef05d711327957e4203315d
Douglas Quintanilha <dougquintanilha@gmail.com>
2017-02-10 19:25:02 -0200

    Added reset and sounds

--- app/js/clock.js
+++ app/js/clock.js
@@ -1,39 +1,45 @@
+const moment = require('moment');
+require('moment-duration-format');
+
 let clock = $(".clock");
 let startButton = $("#start-button");
 let stopButton = $("#stop-button");
+let resetButton = $("#reset-button");
+let sound = new Audio("sounds/drum1.mp3");
 let countdown = null;
+let stopAlarm = null;
 
-let timeArray = clock.text()
-                    .split(":")
-                    .map((item)=>{
-                        return parseInt(item);
-                    });
-let time = {
-    hour: timeArray[0],
-    minute: timeArray[1],
-    second: timeArray[2]
-}
+let duration = moment.duration(clock.text());
 
-let dateTime = new Date();
-dateTime.setHours(time.hour);
-dateTime.setMinutes(time.minute);
-dateTime.setSeconds(time.second);
-
-
-function startTimer(clockId, dateTime){
+function startTimer(clockId, duration){
+    clearInterval(countdown);
     countdown = setInterval(()=>{
-        dateTime.setSeconds(dateTime.getSeconds() - 1);
-        clockId.text(formatTime(dateTime));
+        duration.subtract(1, "s");
+        clockId.text(duration.format("hh:mm:ss",  { trim: false }));
+        if(duration.asSeconds() <= 0){
+            clearInterval(countdown);
+            new Notification("Pomodoro is over");
+            setTimeout(()=>{
+                stopAlarm == true;
+            },5000);
+            sound.play();
+            sound.addEventListener("ended",loopAudio );
+        }
     },1000);
     return countdown;
+}
+
+function loopAudio(){
+        this.currentTime = 0;
+        this.play();
 }
 
 function stopTime(timerId){
     clearInterval(countdown);
 }
 
-function formatTime(dateTime){
-    return dateTime.getHours()+ ":" + dateTime.getMinutes() + ":"+ dateTime.getSeconds();
+function formatDuration(duration){
+    return duration.hours()+ ":" + duration.minutes() + ":"+ duration.seconds();
 }
 
 stopButton.click(()=>{
@@ -41,5 +47,11 @@ stopButton.click(()=>{
 });
 
 startButton.click(()=>{
-    startTimer(clock,dateTime);
+    startTimer(clock,duration);
+});
+
+resetButton.click(()=>{
+    clock.text("00:00:03");
+    duration = moment.duration("00:00:03");
+    sound.removeEventListener("ended", loopAudio);
 });
